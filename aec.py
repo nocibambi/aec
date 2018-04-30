d7db47ec8365e988280ba22d54081b811ab234e6
nocibambi <nocibambi@gmail.com>
2018-04-30 15:17:14 +0200

    First everything

---
+++ aec.py
@@ -0,0 +1,11 @@
+# Atlas of economic coplexity EDA
+
+# Importing packages
+import pandas as pd
+
+#h0 = pd.read_stata("H0_cpy_all.dta")
+#s2 = pd.read_stata("S2_final_cpy_all.dta")
+
+h2016 = pd.read_stata("H0_2016.dta")
+
+print(h0.info())
